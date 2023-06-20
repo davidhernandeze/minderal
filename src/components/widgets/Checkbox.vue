6115d01697d081a3f85b60491a0d2249dbd69514
David Hdz <davidhernandeze@gmail.com>
2023-06-19 19:57:36 -0600

    add checkbox widget

---
+++ src/components/widgets/Checkbox.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="flex justify-center h-full items-center box-border">
+    <div @click.stop="$emit('update', !value)">
+      <div v-if="value">
+        <i class="fa-light fa-square-check h-10 hover:h-12 text-green-400" />
+      </div>
+      <div v-else>
+        <i class="fa-light fa-square h-10 hover:h-12 text-green-400" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+defineEmits(['update'])
+defineProps({
+  value: {
+    type: Boolean,
+    required: true
+  }
+})
+</script>
