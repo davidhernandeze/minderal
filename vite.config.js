dcdfdeb02aa64a028c9cab917d8aee83d7462e9e
David H <davidhernandeze@gmail.com>
2024-11-07 20:45:50 -0600

    add shareable pwa

--- vite.config.js
+++ vite.config.js
@@ -19,30 +19,39 @@ export default defineConfig({
         short_name: 'minderal',
         description: 'Mind mapping tool',
         theme_color: '#1f2937',
-        "background_color": "#f8fff8",
-        icons:  [
+        background_color: '#f8fff8',
+        icons: [
           {
-            "src": "pwa-64x64.png",
-            "sizes": "64x64",
-            "type": "image/png"
+            src: 'pwa-64x64.png',
+            sizes: '64x64',
+            type: 'image/png'
           },
           {
-            "src": "pwa-192x192.png",
-            "sizes": "192x192",
-            "type": "image/png"
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
           },
           {
-            "src": "pwa-512x512.png",
-            "sizes": "512x512",
-            "type": "image/png"
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
           },
           {
-            "src": "maskable-icon-512x512.png",
-            "sizes": "512x512",
-            "type": "image/png",
-            "purpose": "maskable"
+            src: 'maskable-icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
           }
-        ]
+        ],
+        share_target: {
+          action: '/shared-content-receiver/',
+          method: 'GET',
+          params: {
+            title: 'name',
+            text: 'description',
+            url: 'link'
+          }
+        }
       }
     })],
   define: {
