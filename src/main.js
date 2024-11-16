8131def6c1997f89a896eb1e5fbb03031ae5c58a
David H <davidhernandeze@gmail.com>
2024-11-16 08:09:51 -0600

    add sentry

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import Modal from '@/components/Modal.vue'
 import { createPinia } from 'pinia'
 import find from 'pouchdb-find'
 import PouchDB from 'pouchdb-browser'
+import * as Sentry from '@sentry/vue'
 
 PouchDB.plugin(find)
 
@@ -19,3 +20,8 @@ window.onkeydown = function (key) {
     key.preventDefault()
   }
 }
+
+Sentry.init({
+  app,
+  dsn: 'https://08d470a3d023b85f1945caa859f33907@o4506906515406848.ingest.us.sentry.io/4506906544242688'
+})
--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+import { sentryVitePlugin } from '@sentry/vite-plugin'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
@@ -5,59 +6,66 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      workbox: {
-        maximumFileSizeToCacheInBytes: 4000000
-      },
-      manifest: {
-        name: 'Minderal',
-        short_name: 'minderal',
-        description: 'Mind mapping tool',
-        theme_color: '#1f2937',
-        background_color: '#f8fff8',
-        icons: [
-          {
-            src: 'pwa-64x64.png',
-            sizes: '64x64',
-            type: 'image/png'
-          },
-          {
-            src: 'pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          },
-          {
-            src: 'maskable-icon-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'maskable'
-          }
-        ],
-        share_target: {
-          action: '/shared-content-receiver/',
-          method: 'GET',
-          params: {
-            title: 'name',
-            text: 'description',
-            url: 'link'
-          }
+  plugins: [vue(), VitePWA({
+    registerType: 'autoUpdate',
+    workbox: {
+      maximumFileSizeToCacheInBytes: 4000000
+    },
+    manifest: {
+      name: 'Minderal',
+      short_name: 'minderal',
+      description: 'Mind mapping tool',
+      theme_color: '#1f2937',
+      background_color: '#f8fff8',
+      icons: [
+        {
+          src: 'pwa-64x64.png',
+          sizes: '64x64',
+          type: 'image/png'
+        },
+        {
+          src: 'pwa-192x192.png',
+          sizes: '192x192',
+          type: 'image/png'
+        },
+        {
+          src: 'pwa-512x512.png',
+          sizes: '512x512',
+          type: 'image/png'
+        },
+        {
+          src: 'maskable-icon-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'maskable'
+        }
+      ],
+      share_target: {
+        action: '/shared-content-receiver/',
+        method: 'GET',
+        params: {
+          title: 'name',
+          text: 'description',
+          url: 'link'
         }
       }
-    })],
+    }
+  }), sentryVitePlugin({
+    org: 'coworg',
+    project: 'minderal'
+  })],
+
   define: {
     global: 'window'
   },
+
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
     }
+  },
+
+  build: {
+    sourcemap: true
   }
 })
