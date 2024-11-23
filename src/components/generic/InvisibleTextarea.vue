276b5f2160c21f5747f338afea117f5255e0d874
David H <davidhernandeze@gmail.com>
2024-11-23 15:54:08 -0600

    add enrichment text as main text

---
+++ src/components/generic/InvisibleTextarea.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { defineModel } from 'vue'
+import RichTextEditor from '@/components/generic/RichTextEditor.vue'
+
+const value = defineModel()
+
+</script>
+<template>
+  <RichTextEditor
+    v-bind="$attrs"
+    v-model="value"
+    class="resize-none bg-transparent border-none hover:text-gray-50 focus:text-gray-50 focus:outline-none p-0"
+  />
+</template>
