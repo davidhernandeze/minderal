0f35dcb77a8d81c045a5527d02076a8f45f21b18
David Hdz <davidhernandeze@gmail.com>
2025-03-23 20:47:07 -0600

    date improve

--- src/components/widgets/DateForm.vue
+++ src/components/widgets/DateForm.vue
@@ -13,10 +13,10 @@ const props = defineProps({
 
 const emits = defineEmits(['submit'])
 
-const day = ref(null)
-const month = ref(null)
-const year = ref(null)
-const time = ref(null)
+const day = ref(moment(props.doc?.content?.day).toDate() ?? null)
+const month = ref(moment(props.doc?.content?.month).toDate() ?? null)
+const year = ref(moment(props.doc?.content?.year).toDate() ?? null)
+const time = ref(moment(props.doc?.content?.time).toDate() ?? null)
 const form = ref({
   _id: props.doc?._id || null,
   name: props.doc?.name || '',
@@ -52,8 +52,8 @@ async function setFromCurrent() {
 
 <template>
   <form class="text-gray-200 flex flex-col gap-4" @submit.prevent="submit">
-    <div class="flex gap-3">
-      <div class="flex flex-col gap-2 w-[13rem]">
+    <div class="flex flex-col sm:flex-row gap-3">
+      <div class="flex flex-col gap-2 w-full sm:w-[13rem]">
         <label for="year">Year</label>
         <DatePicker id="year" v-model="year" showButtonBar view="year" dateFormat="yy" />
       </div>
@@ -68,7 +68,7 @@ async function setFromCurrent() {
           pt:header:class="!hidden"
         />
       </div>
-      <div class="flex flex-col gap-2 w-16">
+      <div class="flex flex-col gap-2 w-full sm:w-16">
         <label for="day">Day</label>
         <DatePicker
           id="day"
@@ -79,7 +79,7 @@ async function setFromCurrent() {
           pt:header:class="!hidden"
         />
       </div>
-      <div class="flex flex-col gap-2 w-36">
+      <div class="flex flex-col gap-2 w-full sm:w-36">
         <label for="day">Time</label>
         <DatePicker id="day" v-model="time" showButtonBar showTime hourFormat="12" timeOnly />
       </div>
