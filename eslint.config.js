f57c8ab2ccf7404d9462f04af0b4a7a6abb18260
David Hdz <davidhernandeze@gmail.com>
2025-03-25 23:45:29 -0600

    update lint

--- eslint.config.js
+++ eslint.config.js
@@ -1,30 +1,19 @@
 import js from '@eslint/js'
 import pluginVue from 'eslint-plugin-vue'
-import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
+import prettierConfig from '@vue/eslint-config-prettier'
 
 export default [
   {
     name: 'app/files-to-lint',
-    files: ['**/*.{js,mjs,jsx,vue}'],
+    files: ['**/*.{js,mjs,jsx,vue}']
   },
 
   {
     name: 'app/files-to-ignore',
-    ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
+    ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**']
   },
 
   js.configs.recommended,
-  ...pluginVue.configs['flat/essential'],
-  skipFormatting,
-  {
-    rules: {
-      'skipBlankLines': 'off',
-      'no-new': 'off',
-      'vue/multi-word-component-names': 'off',
-      "vue/block-order": ["error", {
-        "order": [ "script", "template" , "style" ]
-      }],
-      'camelcase': 'off'
-    },
-  }
+  ...pluginVue.configs['flat/recommended'],
+  prettierConfig
 ]
--- vite.config.js
+++ vite.config.js
@@ -3,79 +3,81 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 import { VitePWA } from 'vite-plugin-pwa'
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), tailwindcss(), VitePWA({
-    registerType: 'autoUpdate',
-    workbox: {
-      maximumFileSizeToCacheInBytes: 4000000
-    },
-    manifest: {
-      name: 'Minderal',
-      short_name: 'minderal',
-      description: 'Mind mapping tool',
-      theme_color: '#1f2937',
-      background_color: '#f8fff8',
-      icons: [
-        {
-          src: 'pwa-64x64.png',
-          sizes: '64x64',
-          type: 'image/png'
+  plugins: [
+    vue(),
+    tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      workbox: {
+        maximumFileSizeToCacheInBytes: 4000000,
+      },
+      manifest: {
+        name: 'Minderal',
+        short_name: 'minderal',
+        description: 'Mind mapping tool',
+        theme_color: '#1f2937',
+        background_color: '#f8fff8',
+        icons: [
+          {
+            src: 'pwa-64x64.png',
+            sizes: '64x64',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'maskable-icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+        ],
+        share_target: {
+          action: '/shared-content-receiver/',
+          method: 'GET',
+          params: {
+            title: 'name',
+            text: 'description',
+            url: 'link',
+          },
         },
-        {
-          src: 'pwa-192x192.png',
-          sizes: '192x192',
-          type: 'image/png'
-        },
-        {
-          src: 'pwa-512x512.png',
-          sizes: '512x512',
-          type: 'image/png'
-        },
-        {
-          src: 'maskable-icon-512x512.png',
-          sizes: '512x512',
-          type: 'image/png',
-          purpose: 'maskable'
-        }
-      ],
-      share_target: {
-        action: '/shared-content-receiver/',
-        method: 'GET',
-        params: {
-          title: 'name',
-          text: 'description',
-          url: 'link'
-        }
-      }
-    }
-  }), sentryVitePlugin({
-    org: 'coworg',
-    project: 'minderal',
-    authToken: process.env.SENTRY_AUTH_TOKEN,
-  })],
+      },
+    }),
+    sentryVitePlugin({
+      org: 'coworg',
+      project: 'minderal',
+      authToken: process.env.SENTRY_AUTH_TOKEN,
+    }),
+  ],
 
   define: {
-    global: 'window'
+    global: 'window',
   },
 
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src')
-    }
+      '@': path.resolve(__dirname, './src'),
+    },
   },
   envPrefix: ['VITE_', 'TAURI_ENV_*'],
   build: {
     // Tauri uses Chromium on Windows and WebKit on macOS and Linux
-    target:
-      process.env.TAURI_ENV_PLATFORM == 'windows'
-        ? 'chrome105'
-        : 'safari13',
+    target: process.env.TAURI_ENV_PLATFORM == 'windows' ? 'chrome105' : 'safari13',
     // don't minify for debug builds
     minify: !process.env.TAURI_ENV_DEBUG ? 'esbuild' : false,
     // produce sourcemaps for debug builds
-    sourcemap: true
+    sourcemap: true,
   },
 })
